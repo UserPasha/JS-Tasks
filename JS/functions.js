df50ec6321f934fbc515ca1abd63d58e839a2134
UserPasha <pasharoma13@gmail.com>
2022-03-11 22:23:06 +0300

    add new cw tasks

---
+++ JS/functions.js
@@ -0,0 +1,21 @@
+function sequence(x) {
+    let a=[]
+    let i=0
+    while (x>0){
+      x=x-1;
+      i++;
+      a.push(i)
+      }
+     return a.sort()
+   }
+
+   const num = 8
+   console.log(sequence(8))
+
+
+   // timmy
+   // dogs
+   // big
+   // bigger
+
+   // &lt;      (<)
