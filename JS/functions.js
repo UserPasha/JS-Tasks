d986c6dc8303500ac079472486d0ef80d4c430ca
UserPasha <pasharoma13@gmail.com>
2022-04-15 21:03:01 +0300

    add new 6 kyo tasks

--- JS/functions.js
+++ JS/functions.js
@@ -12,13 +12,13 @@ function sequence(x) {
 const num = 8
 console.log(sequence(8))
 
-function dateTest(y,m,d){
-const a = new Date(y, m-1, d)
-const b = new Date()
-let days = `your are ${Math.floor((b.getTime()-a.getTime())/ (1000*60*60*24))} days`
-let hours = `your are ${Math.floor((b.getTime()-a.getTime())/ (1000*60*60))} hours`
-let minutes = `your are ${Math.floor((b.getTime()-a.getTime())/ (1000*60))} minutes`
-return minutes
+function dateTest(y, m, d) {
+  const a = new Date(y, m - 1, d)
+  const b = new Date()
+  let days = `your are ${Math.floor((b.getTime() - a.getTime()) / (1000 * 60 * 60 * 24))} days`
+  let hours = `your are ${Math.floor((b.getTime() - a.getTime()) / (1000 * 60 * 60))} hours`
+  let minutes = `your are ${Math.floor((b.getTime() - a.getTime()) / (1000 * 60))} minutes`
+  return minutes
 }
 
 console.log(dateTest(1981, 4, 8))
@@ -31,57 +31,94 @@ console.log(dateTest(1981, 4, 8))
 // &lt;      (<)
 
 let importantSi = [
-  {
-    book: "read the book",
-    network: "Social Net on JS",
-    homework: "Another Ignat's homework",
-    layout: "One lesson with Prokopchuck",
-  }
+
+  "read the book",
+  "Social Net on JS",
+  "Another Ignat's homework",
+  "One lesson with Prokopchuck",
+
 ]
 
 let importantMi = [
-  {
-    withReact: "work with project. add nav or structure or css",
-    brouser: "claer one tab",
-    react: "kabzda like easy",
-    native: "lesson native",
-    MIU: "walking on miu",
-  }
+
+  "work with project. add nav or structure or css",
+  "claer one tab",
+  "kabzda like easy",
+  "lesson native",
+  "walking on miu",
+
 ]
 
 let importantJu = [
-  {
-    codewars: "Solve task on CodeWars",
-    instagramm: "Check some saves",
-    css: "play a css game",
-  }
+
+  "Solve task on CodeWars",
+  "Check some saves",
+  "play a css game",
+
 ]
 let testArr = [
   'importantSi', 'importantMi', 'importantJu'
 ]
-//let randomWithSi = Math.random(testArr.length.Math.random()
-let a = testArr.length
-let b = Math.round(Math.random() * 3)
-let c = Math.round(Math.random() * 4)
-let d = Math.round(Math.random()* 2)
 
-let arrayOfValuesFromImoptanSi = Object.values(importantSi[0]) //array
-let randomOfImportantSi = arrayOfValuesFromImoptanSi[b]
-let arrayOfValuesFromImportantMi = Object.values(importantMi[0])
-let randomOfImportantMi = arrayOfValuesFromImportantMi[c]
-let arrayOfValuesFromImportantJu = Object.values(importantJu[0])
-let randomOfImportantJu = arrayOfValuesFromImportantJu[d]
+let randomOfImportantSi = importantSi[Math.floor(Math.random() * importantSi.length)]
+let randomOfImportantMi = importantMi[Math.floor(Math.random() * importantMi.length)]
+let randomOfImportantJu = importantJu[Math.floor(Math.random() * importantJu.length)]
 
 
-//let x = Object.values(importantSi[0])[b]
 
+console.log(randomOfImportantJu + "  /  " + randomOfImportantMi + "  /  " + randomOfImportantSi)
 
-function testFunction() {
+let a = 1
+let b = 2
+let c = a + b++
+let d = a + ++b
+console.log(c) // 3 (1+2)
+console.log(d) // 5 (1+4(1+3))
 
-  console.log(randomOfImportantJu + "  /  " + randomOfImportantMi + "  /  " + randomOfImportantSi)
+"01|15|59, 1|47|16, 01|17|20, 1|32|34, 2|17|17"
 
-
-  //return testArr[b]
+function stat(st) {
+  if(st===""){
+    return ""
+  }
+  let a = st.split(",")
+  let b = a.map(m => m.split("|"))
+  let c = b.map(b => (+b[0] * 3600) + (+b[1] * 60) + (+b[2]))
+  let d = c.sort((a,b)=>a-b)
+  let e = Math.floor((d[d.length - 1] - d[0]) / 3600)
+  e<10? e="0"+e+"":e;
+  let f = Math.floor(((d[d.length - 1] - d[0]) % 3600) / 60)
+  f<10? f="0"+f+"":f;
+  let g = ((d[d.length - 1] - d[0]) % 3600) % 60
+  g<10? g="0"+g+"":g;
+  let range = "" + e + "|" + f + "|" + g + ""
+  let h = d.reduce((a, b) => a + b) / d.length 
+  let i = Math.floor(h / 3600)
+  i<10? i="0"+i+"":i;
+  let l = Math.floor(h % 3600 / 60)
+  l<10? l="0"+l+"":l;
+  let k = Math.floor(h % 3600 % 60)
+  k<10? k="0"+k+"":k;
+  let Average = "" + i + "|" + l + "|" + k + ""
+  let m
+  if (d.length % 2 > 0) {
+    m = d[(d.length-1) / 2]
+  }else {
+    m= (d[d.length / 2]+d[Math.floor((d.length-1) / 2)])/2
+  }
+  let n = Math.floor(m / 3600)
+  let y = m-n*3600
+  n<10? n="0"+n+"":n;
+  let x = Math.floor(y / 60)
+  x<10? x="0"+x+"":x;
+  y<10? y="0"+y+"":y;
+  let z = Math.floor(y-x*60)
+  z<10? z="0"+z+"":z;
+  let median = "" + n + "|" + x + "|" + z + ""
+  return "Range: "+range+" Average: "+Average+" Median: "+median+""
 }
+console.log(stat("02|15|59, 2|47|16, 02|17|20, 2|32|34, 2|17|17, 2|22|00, 2|31|41"))
 
-testFunction();
+1|1|18 
+1|38|4
+1|38|4
